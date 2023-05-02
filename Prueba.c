c84c6f818c195e7ff0b7e7ef39c4d6960ee47306
JoanCastroo <131194146+JoanCastroo@users.noreply.github.com>
2023-05-02 12:55:48 -0500

    Add files via upload
    Aplique una función "En este caso while", que me permitió seguir con la solucion del programa mediendome de operaciones algebraicas para la solventacion del mismo y un printf que me ayuda a mostrarle al usuario el resultado

--- Prueba.c
+++ Prueba.c
@@ -6,6 +6,13 @@ int number, suma=0, cont=0;
 printf("Introduce un número entero: ");
     scanf("%d", &number);
 
+    while (number != 0) {
+        suma += number % 10;
+        number /= 10;
+        cont++;
+    }
+
+    printf("El número tiene %d dígitos y su suma es %d.\n", cont, suma);
 }
 
 
